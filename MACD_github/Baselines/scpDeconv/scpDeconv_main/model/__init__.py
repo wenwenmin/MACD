6ce3b664d47ae5f57909f30d4c5921da7858f554
linhuang888 <161809052+linhuang888@users.noreply.github.com>
2024-07-24 17:30:29 +0800

    Delete MACD_github directory

--- MACD_github/Baselines/scpDeconv/scpDeconv_main/model/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from Baselines.scpDeconv.scpDeconv_main.model.AEimpute_model import *
-from Baselines.scpDeconv.scpDeconv_main.model.DANN_model import *
-from Baselines.scpDeconv.scpDeconv_main.model.utils import *
